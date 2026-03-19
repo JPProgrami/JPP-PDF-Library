88438b8e483b2863b33cd23fd649a74eb3f0213d
JP20221069 <pj20221069@student.fon.bg.ac.rs>
2026-03-19 04:00:17 +0100

    Added PDF Text handling classes.

---
+++ JPP.PDFLibrary/PDFTextDocument.cs
@@ -0,0 +1,54 @@
+﻿using PDFiumSharp;
+using PDFiumSharp.Types;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.PDFLibrary
+{
+    public class PDFTextDocument
+    {
+        public List<PDFTextPage> Pages { get; }
+
+        public string Text { get
+            {
+                StringBuilder sb = new StringBuilder();
+                int pgcount = 0;
+                foreach(PDFTextPage p in this.Pages)
+                {
+                    try
+                    {
+                        sb.Append(p.Text);
+                    }
+                    catch(Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message + "\n PANIC!!! AT PDF PAGE " + pgcount);
+                    }
+                    sb.Append(Environment.NewLine+Environment.NewLine+Environment.NewLine + "-- PAGE " +(p.Index+1)+" / "+this.Pages.Count+" --"+Environment.NewLine+Environment.NewLine+Environment.NewLine);
+                    pgcount++;
+                }
+                return sb.ToString();
+            } }
+
+        public PDFTextDocument(PdfDocument doc)
+        {
+            List<PDFTextPage> pag = new List<PDFTextPage>();
+            foreach (PdfPage p in doc.Pages)
+            {
+                FPDF_PAGE temp_page = PDFium.FPDF_LoadPage(doc.PDFiumDocument, p.Index);
+                FPDF_TEXTPAGE text_page = PDFium.FPDFText_LoadPage(temp_page);
+                pag.Add(new PDFTextPage(text_page, p.Width, p.Height, p.Index));
+            }
+            this.Pages = pag;
+        }
+
+        public PDFTextDocument(string path, string password = null) : this(new PdfDocument(path, password))
+        {
+
+        }
+
+    }
+}
---
+++ JPP.PDFLibrary/PDFTextPage.cs
@@ -0,0 +1,34 @@
+﻿using PDFiumSharp;
+using PDFiumSharp.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPP.PDFLibrary
+{
+    public class PDFTextPage
+    {
+        public string Text { get { return PDFium.FPDFText_GetBoundedText(this.fPDF_TEXTPAGE, 0, 0, this.width, this.height); } }
+
+        public double Width { get => this.width; set => this.width = value; }
+        public double Height { get => this.height; set => this.height = value; }
+
+        public int Index => this.index;
+
+        private FPDF_TEXTPAGE fPDF_TEXTPAGE;
+        private double width;
+        private double height;
+        private readonly int index;
+
+        public PDFTextPage(FPDF_TEXTPAGE fPDF_TEXTPAGE,double width,double height,int index)
+        {
+            this.fPDF_TEXTPAGE = fPDF_TEXTPAGE;
+            this.width = width;
+            this.height = height;
+            this.index = index;
+        }
+
+    }
+}
